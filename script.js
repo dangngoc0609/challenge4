c91ca7185dcfd26044c1e6b058109f9d3fc37522
Tho Dang <emilydangevc@gmail.com>
2021-05-31 17:43:47 -0700

    added timeInterval

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+var timerEl = document.querySelector("span")
 var startButton = document.querySelector(".button")
 var startContainer = document.querySelector("header")
 var questionArea = document.querySelector("section")
@@ -13,8 +14,41 @@ var anwserButton1 = document.querySelector("#anwserbutton1")
 var anwserButton2 = document.querySelector("#anwserbutton2")
 var anwserButton3 = document.querySelector("#anwserbutton3")
 var anwserButton4 = document.querySelector("#anwserbutton4")
+var timerInterval 
+var timeleft = 60
+function timer(){
+    
+    timerInterval = setInterval(() => {
+     if (timeleft > 0){
+         timeleft--
+     timerEl.innerText = timeleft    
+     }else{
+         clearInterval(timerInterval)
+     }   
+    }, 1000);
+}
+
+
 function checkAnwser(event){
+  var userchoices = event.target.innerText
+if (userchoices === questions[currentQuestionIndex].correctAwnser){
+   questionText.innerText = "corrected"
     currentQuestionIndex++
+}else{
+    questionText.innerText = "Incorrected"
+    currentQuestionIndex++
+}
+setTimeout(function(){
+    if (currentQuestionIndex > questions.length - 1){
+        clearInterval(timerInterval)
+    }else{
+        displayQuestion ()
+    }
+
+    
+},1000)
+
+    
     console.log(event)
 console.log(event.target.innerText)
 }
@@ -31,10 +65,10 @@ anwserButton2.addEventListener("click",checkAnwser)
 anwserButton3.addEventListener("click",checkAnwser)
 anwserButton4.addEventListener("click",checkAnwser)
 startButton.addEventListener("click", function () {
-
-    console.log("hello")
+timer()
+   
     startContainer.style.display = "none"
     questionArea.style.display = "block"
    
 })
-displayQuestion ()
+displayQuestion ();
